e9516524e0390dbaa0c463de85f6219f5c87aa2f
Ion <ikoniaris@gmail.com>
2013-07-30 19:37:43 +0300

    German language

---
+++ include/languages/lang.de.php
@@ -0,0 +1,46 @@
+﻿<?php
+#Package: Kippo-Graph
+#Version: 0.7.6
+#Author: ikoniaris
+#Website: bruteforce.gr/kippo-graph
+#Translation: Dario Rugani
+
+//INDEX.PHP STRINGS
+//MORE TODO
+define('LATEST_VERSION', ' - Sie benutzen die <strong>neuste</strong> Version!');
+define('NEW_VERSION_AVAILABLE', ' - Es steht eine <strong>neue</strong> Version zum <a href="http://bruteforce.gr/kippo-graph" target="_blank">Download</a> bereit!');
+
+//KIPPO-GRAPH-GENERATOR.PHP STRINGS
+define('TOP_10_PASSWORDS', 'Top 10 benutzte Passwörter');
+define('TOP_10_USERNAMES', 'Top 10 benutzte Benutzernamen');
+define('TOP_10_COMBINATIONS', 'Top 10 Benutzername - Passwort Kombinationen');
+define('AUTH_FAIL', 'Fehlschlag');
+define('AUTH_SUCCESS', 'Erfolg');
+define('OVERALL_SUCCESS_RATIO', 'Insgesammte Erfolgsquote');
+define('MOST_SUCCESSFUL_LOGINS_PER_DAY', 'Die meisten erfolgreichen Logins pro Tag (Top 20)');
+define('SUCCESSES_PER_DAY', 'Erfolge pro Tag');
+define('SUCCESSES_PER_WEEK', 'Erfolge pro Woche');
+define('NUMBER_OF_CONNECTIONS_PER_UNIQUE_IP', 'Anzahl der Verbindugnen pro eindeutige IP(Top 10)');
+define('SUCCESSFUL_LOGINS_FROM_SAME_IP', 'Erfolgreiche Anmeldungen von derselben IP (Top 20)');
+define('MOST_PROBES_PER_DAY', 'Die meisten Versuche pro Tag (Top 20)');
+define('PROBES_PER_DAY', 'Versuche pro Tag');
+define('PROBES_PER_WEEK', 'Versuche pro Woche');
+define('TOP_10_SSH_CLIENTS', 'Top 10 SSH Clienten');
+
+//KIPPO-GRAPH.PHP STRINGS
+//TODO
+
+//KIPPO-INPUT.PHP STRINGS
+//MORE TODO
+define('HUMAN_ACTIVITY_BUSIEST_DAYS', 'Menschliche Aktivität an dem belebtesten Tagen (Top 20)');
+define('HUMAN_ACTIVITY_PER_DAY', 'Menschliche Aktivität pro Tag');
+define('HUMAN_ACTIVITY_PER_WEEK', 'Menschliche Aktivität pro Woche');
+define('TOP_10_INPUT_OVERALL', 'Top 10 Eingaben (insgesamt)');
+define('TOP_10_SUCCESSFUL_INPUT', 'Top 10 erfolgreicher Eingaben');
+define('TOP_10_FAILED_INPUT', 'Top 10 fehlgeschlagener Eingaben');
+
+//KIPPO-GEO.PHP STRINGS
+//MORE TODO
+define('NUMBER_OF_CONNECTIONS_PER_UNIQUE_IP_CC', 'Anzahl der Verbindungnen pro eindeutige IP(Top 10) + Ländercodes');
+define('NUMBER_OF_CONNECTIONS_PER_COUNTRY', 'Anzahl der Verbindungen pro Land');
+?>
