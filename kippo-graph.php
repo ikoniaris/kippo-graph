3d3f76488c55f3a785203c4cf12f972d92234b0f
Ion <ikoniaris@gmail.com>
2014-02-06 17:01:38 +0200

    Upload version 0.9.1

--- kippo-graph.php
+++ kippo-graph.php
@@ -17,7 +17,7 @@
 <!-- ####################################################################################################### -->
 <div class="wrapper">
   <div id="topbar">
-    <div class="fl_left">Version: 0.9 | Website: <a href="http://bruteforce.gr/kippo-graph">bruteforce.gr/kippo-graph</a></div>
+    <div class="fl_left">Version: 0.9.1 | Website: <a href="http://bruteforce.gr/kippo-graph">bruteforce.gr/kippo-graph</a></div>
     <br class="clear" />
   </div>
 </div>
