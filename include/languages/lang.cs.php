37ddd37592a68470a26ba7f14f84c5dde773e8b8
ikoniaris <ikoniaris@gmail.com>
2014-07-10 22:05:13 +0100

    Add Czech language file

---
+++ include/languages/lang.cs.php
@@ -0,0 +1,41 @@
+<?php
+
+//INDEX.PHP STRINGS
+//MORE TODO
+define('LATEST_VERSION', ' - Používáte <strong>nejnovější</strong> verzi!');
+define('NEW_VERSION_AVAILABLE', ' - Je dostupná <strong>nová</strong> verze ke <a href="http://bruteforce.gr/kippo-graph" target="_blank">stažení</a>!');
+
+//KIPPO-GRAPH-GENERATOR.PHP STRINGS
+define('TOP_10_PASSWORDS', 'Top 10 zkoušených hesel');
+define('TOP_10_USERNAMES', 'Top 10 zkoušených uživatelů');
+define('TOP_10_COMBINATIONS', 'Top 10 kombinací uživatel-heslo');
+define('AUTH_FAIL', 'Neúspěšné');
+define('AUTH_SUCCESS', 'Úspěšné');
+define('OVERALL_SUCCESS_RATIO', 'Celkový poměr úspěšnosti');
+define('MOST_SUCCESSFUL_LOGINS_PER_DAY', 'Nejvíce úspěšných přihlášení za den (Top 20)');
+define('SUCCESSES_PER_DAY', 'Úspěchy za den');
+define('SUCCESSES_PER_WEEK', 'Úspěchy za týden');
+define('NUMBER_OF_CONNECTIONS_PER_UNIQUE_IP', 'Počet připojení podle unikátní IP (Top 10)');
+define('SUCCESSFUL_LOGINS_FROM_SAME_IP', 'Úspěšné přihlášení od stejné IP (Top 20)');
+define('MOST_PROBES_PER_DAY', 'Nejvíce sond za den (Top 20)');
+define('PROBES_PER_DAY', 'Sond za den');
+define('PROBES_PER_WEEK', 'Sond za týden');
+define('TOP_10_SSH_CLIENTS', 'Top 10 SSH klientů');
+
+//KIPPO-GRAPH.PHP STRINGS
+//TODO
+
+//KIPPO-INPUT.PHP STRINGS
+//MORE TODO
+define('HUMAN_ACTIVITY_BUSIEST_DAYS', 'Lidská činnost - nejrušnější den (Top 20)');
+define('HUMAN_ACTIVITY_PER_DAY', 'Lidská činnost za den');
+define('HUMAN_ACTIVITY_PER_WEEK', 'Lidská činnost za týden');
+define('TOP_10_INPUT_OVERALL', 'Top 10 vstupů (celkově)');
+define('TOP_10_SUCCESSFUL_INPUT', 'Top 10 úspěšných vstupů');
+define('TOP_10_FAILED_INPUT', 'Top 10 neúspěšných vstupů');
+
+//KIPPO-GEO.PHP STRINGS
+//MORE TODO
+define('NUMBER_OF_CONNECTIONS_PER_UNIQUE_IP_CC', 'Počet připojení podle unikátní IP (Top 10) + Kódy zemí');
+define('NUMBER_OF_CONNECTIONS_PER_COUNTRY', 'Počet připojení podle země');
+?>
