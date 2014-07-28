1d5fab37ccd575614b93b0fa6f5051ddd92046b9
ikoniaris <ikoniaris@gmail.com>
2014-07-29 00:23:50 +0100

    Convert KippoIP class to RedBeanPHP and tidy up query

--- class/KippoIP.class.php
+++ class/KippoIP.class.php
@@ -1,45 +1,41 @@
 <?php
+require_once(DIR_ROOT . '/include/rb.php');
 
 class KippoIP
 {
-    private $db_conn;
 
     function __construct()
     {
         //Let's connect to the database
-        $this->db_conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT); //host, username, password, database, port
-
-        if (mysqli_connect_errno()) {
-            echo 'Error connecting to the database: ' . mysqli_connect_error();
-            exit();
-        }
+        R::setup('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
     }
 
     function __destruct()
     {
-        $this->db_conn->close();
+        R::close();
     }
 
     public function printOverallIpActivity()
     {
-        $db_query = 'SELECT * FROM (SELECT ip, MAX(starttime), COUNT(DISTINCT sessions.id) '
-            . "FROM sessions GROUP BY ip) A "
-            . "LEFT JOIN (SELECT sessions.ip, MAX(success) "
-            . "FROM sessions, auth "
-            . "WHERE sessions.id = auth.session "
-            . "GROUP BY ip) B on A.ip = B.ip "
-            . "ORDER BY A.ip";
-        $result = $this->db_conn->query($db_query);
-        //echo 'Found '.$result->num_rows.' records';
+        $db_query = "SELECT A.*, B.success FROM (
+            SELECT ip, MAX(starttime) as starttime, COUNT(DISTINCT sessions.id) as sessions
+            FROM sessions GROUP BY ip) A
+            LEFT JOIN (
+              SELECT sessions.ip, MAX(success) as success
+              FROM sessions, auth
+              WHERE sessions.id = auth.session
+              GROUP BY ip) B on A.ip = B.ip
+            ORDER BY A.ip";
 
-        if ($result->num_rows > 0) {
+        $rows = R::getAll($db_query);
 
+        if (count($rows)) {
             echo '<p>Click column heads to sort data, rows to display attack details.</p>';
 
             //We create a skeleton for the table
             echo '<table id="Overall-IP-Activity" class="tablesorter"><thead>';
             echo '<tr class="dark">';
-            echo '<th colspan="4">Total identified IP addresses: ' . $result->num_rows . '</th>';
+            echo '<th colspan="4">Total identified IP addresses: ' . count($rows) . '</th>';
             echo '</tr>';
             echo '<tr class="dark">';
             echo '<th>IP address</th>';
@@ -50,14 +46,13 @@ class KippoIP
 
             //For every row returned from the database we add a new point to the dataset,
             //and create a new table row with the data as columns
-            while ($row = $result->fetch_array(MYSQLI_BOTH)) {
+            foreach ($rows as $row) {
+                $success = is_null($row['success']) ? 'N/A' : $row['success'];
+                $timestamp = is_null($row['starttime']) ? 'N/A' : $row['starttime'];
 
-                $success = is_null($row['MAX(success)']) ? 'N/A' : $row['MAX(success)'];
-                $timestamp = is_null($row['MAX(starttime)']) ? 'N/A' : $row['MAX(starttime)'];
-
-                echo '<tr class="light word-break" onclick=\'getIPinfo("' . $row['0'] . '")\'>';
-                echo '<td>' . $row['0'] . '</td>';
-                echo '<td>' . $row['COUNT(DISTINCT sessions.id)'] . '</td>';
+                echo '<tr class="light word-break" onclick=\'getIPinfo("' . $row['ip'] . '")\'>';
+                echo '<td>' . $row['ip'] . '</td>';
+                echo '<td>' . $row['sessions'] . '</td>';
                 echo '<td>' . $success . '</td>';
                 echo '<td>' . $timestamp . '</td>';
                 echo '</tr>';
