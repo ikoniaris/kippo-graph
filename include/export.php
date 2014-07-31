683065c830217820f114e48ab65edc68436f7dcb
ikoniaris <ikoniaris@gmail.com>
2014-07-31 22:01:00 +0100

    Convert export.php (CSV) to RedBeanPHP

--- include/export.php
+++ include/export.php
@@ -10,33 +10,26 @@ header('Content-Disposition: attachment;filename="' . $fileName . '"');
 header('Cache-Control: max-age=0');
 
 require_once('../config.php');
-require_once('../include/sql.php'); // this has all the SQL Statements named as per Query String below
-require_once('../include/misc/xss_clean.php');
+require_once(DIR_ROOT . '/include/rb.php');
+require_once(DIR_ROOT . '/include/sql.php'); // this has all the SQL Statements named as per Query String below
+require_once(DIR_ROOT . '/include/misc/xss_clean.php');
 
 //Valid queries should be usernames, passwords, IP's optional query should be Limit.
 
-
 //Let's connect to the database
-$db_conn = new mysqli(DB_HOST, DB_USER, DB_PASS, DB_NAME, DB_PORT); //host, username, password, database, port
-
-if (mysqli_connect_errno()) {
-    echo 'Error connecting to the database: ' . mysqli_connect_error();
-    exit();
-}
-
+R::setup('mysql:host=' . DB_HOST . ';port=' . DB_PORT . ';dbname=' . DB_NAME, DB_USER, DB_PASS);
 
 // create the varaiable name from the URL Query string which should match SQL.php, than pass it as db_query
 $db_query = ${"db_" . xss_clean($_GET['type'])};
 
-
-$result = $db_conn->query($db_query);
+$rows = R::getAll($db_query);
 
 $first = true; // flag for column titeles
 
 // open file without writing to disk
 $out = fopen('php://output', 'w');
 
-while ($row = $result->fetch_array(MYSQLI_NUM)) {
+foreach ($rows as $row) {
     if ($first) {
         $titles = array();
         foreach ($row as $key => $val) {
