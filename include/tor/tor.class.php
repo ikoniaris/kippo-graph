4596312a1726024b0bdcf551cec37a7d51e81e49
ikoniaris <ikoniaris@gmail.com>
2015-02-01 16:38:19 +0000

    Add Tor class

---
+++ include/tor/tor.class.php
@@ -0,0 +1,45 @@
+<?php
+
+class Tor
+{
+    private $exit_node_array;
+
+    function __construct()
+    {
+        if ($this->_TorBulkExitListWorking('https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.2.3.4')) {
+            $exit_node_array = file('https://check.torproject.org/cgi-bin/TorBulkExitList.py?ip=1.2.3.4');
+            file_put_contents(DIR_ROOT . '/include/tor/tor_exit_node_list.txt', $exit_node_array);
+        } else {
+            $exit_node_array = file(DIR_ROOT . '/include/tor/tor_exit_node_list.txt');
+        }
+
+        for ($i = 0; $i < 3; $i++)
+            array_shift($exit_node_array);
+
+        $this->exit_node_array = $exit_node_array;
+    }
+
+    public function isTorExitNode($ip)
+    {
+        return in_array($ip, $this->exit_node_array);
+    }
+
+    function _TorBulkExitListWorking($url)
+    {
+        $ch = curl_init($url);
+        curl_setopt($ch, CURLOPT_NOBODY, true);
+        curl_exec($ch);
+
+        $code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+
+        if ($code == 200)
+            $status = true;
+        else
+            $status = false;
+
+        curl_close($ch);
+        return $status;
+    }
+}
+
+?>
