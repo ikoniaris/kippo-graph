2dbc79b36f6668c03123e0e4e0348c19f8074f2b
ikoniaris <ikoniaris@gmail.com>
2015-02-01 13:21:35 +0000

    Bump version to 1.4

--- include/play.php
+++ include/play.php
@@ -22,7 +22,7 @@
 <!-- ####################################################################################################### -->
 <div class="wrapper">
     <div id="topbar">
-        <div class="fl_left">Version: 1.3 | Website: <a href="http://bruteforce.gr/kippo-graph">bruteforce.gr/kippo-graph</a>
+        <div class="fl_left">Version: 1.4 | Website: <a href="http://bruteforce.gr/kippo-graph">bruteforce.gr/kippo-graph</a>
         </div>
         <br class="clear"/>
     </div>
