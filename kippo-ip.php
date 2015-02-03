65fca7f5a9a6f9e5a19a5f4b5f3677fc47cee7da
zhardie <zackhardie@gmail.com>
2015-02-03 13:16:02 -0500

    Update kippo-ip.php
    This update just changes the banner link from kippo-graph.php to kippo-graph.html.

--- kippo-ip.php
+++ kippo-ip.php
@@ -32,7 +32,7 @@
     <div id="topnav">
         <ul class="nav">
             <li><a href="index.php">Homepage</a></li>
-            <li><a href="kippo-graph.php">Kippo-Graph</a></li>
+            <li><a href="kippo-graph.html">Kippo-Graph</a></li>
             <li><a href="kippo-input.php">Kippo-Input</a></li>
             <li><a href="kippo-playlog.php">Kippo-Playlog</a></li>
             <li class="active"><a href="kippo-ip.php">Kippo-Ip</a></li>
