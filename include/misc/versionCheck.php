059220b6fddd282f2e880b0f0cb9d869b705c99f
ikoniaris <ikoniaris@gmail.com>
2015-02-04 00:19:36 +0000

    Bump version 1.5

--- include/misc/versionCheck.php
+++ include/misc/versionCheck.php
@@ -6,7 +6,7 @@
 define('REMOTE_VERSION', 'http://bruteforce.gr/kippo-graph-version.txt');
 
 // this is the version of the deployed script
-define('VERSION', '1.4.2');
+define('VERSION', '1.5');
 
 function isUpToDate()
 {
